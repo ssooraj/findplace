b23eb968ce392291df386a5819b6fe5bfe3370b1
sharathkumar <sharathkumar@qburst.com>
2013-11-25 14:43:15 +0530

    Code Cleanup

--- lib/findplace.rb
+++ lib/findplace.rb
@@ -9,30 +9,30 @@ class FindPlace
     Class method that reads the client IP,decodes it to find the location coordinates using GeoCoder 
     Uses the decoded coordinates and Client Request Type to find locations using GooglePlaces API
 =end
-    def self.search_nearby(google_api_key,client_ip,type)
-        #ip which we used to test  
-        #client_ip = '115.248.107.225'  
-        coordinates=Geocoder.coordinates(client_ip)
-        begin
-            if coordinates.nil?
-               searched_places = ["No Results,Request Time Out"]
-            else
-               searched_places= Array.new
-               lng=coordinates[1].to_f
-               lat=coordinates[0].to_f
-               client = GooglePlaces::Client.new(google_api_key)
-               results = client.spots( coordinates[0].to_f, coordinates[1].to_f, :radius=> '5000', :types => type)
-                if results.length == 0
-                    searched_places = ["Sorry...........No results for Your Search"]
-                else  
-                    results.length.times do |i|
-                        searched_places[i] = results[i].name, results[i].vicinity, results[i].lng, results[i].lat     
-                    end 
-                end  
-            end
-        rescue 
-            searched_places = ["Invalid API Key"]
-        end    
-        return searched_places
+  def self.search_nearby(google_api_key,client_ip,type)
+    #reads the client_ip from request
+    #private key provided by google should be given as google_api_key  
+    coordinates=Geocoder.coordinates(client_ip)
+    begin
+      if coordinates.nil?
+        searched_places = ["No Results,Request Time Out"]
+      else
+        searched_places= Array.new
+        lng=coordinates[1].to_f
+        lat=coordinates[0].to_f
+        client = GooglePlaces::Client.new(google_api_key)
+        results = client.spots( coordinates[0].to_f, coordinates[1].to_f, :radius=> '5000', :types => type)
+          if results.length == 0
+              searched_places = ["Sorry...........No results for Your Search"]
+          else  
+              results.length.times do |i|
+                  searched_places[i] = results[i].name, results[i].vicinity, results[i].lng, results[i].lat     
+              end 
+          end  
+      end
+    rescue 
+      searched_places = ["Invalid API Key"]
+    end    
+      return searched_places
     end
 end
--- test/test_findplace.rb
+++ test/test_findplace.rb
@@ -4,10 +4,10 @@ class TestFindplace < Test::Unit::TestCase
   # should "probably rename this file and start testing for real" do
   #   flunk "hey buddy, you should probably rename this file and start testing for real"
   # end
-	def test_geocoder_coordinates
-    	coord = [13.0833, 80.2833]
-      @coordinates = Geocoder.coordinates('115.248.107.225')
-      @coordinates[0].to_s == coord[0].to_s
+  def test_geocoder_coordinates
+    coord = [13.0833, 80.2833]
+    @coordinates = Geocoder.coordinates('115.248.107.225')
+    @coordinates[0].to_s == coord[0].to_s
   end    
 
 end
